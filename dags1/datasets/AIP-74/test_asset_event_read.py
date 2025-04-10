6eeacf747833702207f8ffb73685e32359dbdad2
Atul Singh <atulsingh@Atuls-MacBook-Pro.local>
2025-04-10 14:02:09 +0530

    vc2

--- dags1/datasets/AIP-74/test_asset_event_read.py
+++ dags1/datasets/AIP-74/test_asset_event_read.py
@@ -1,38 +1,40 @@
-import datetime
+from datetime import datetime, timedelta
 
 from airflow.sdk import Asset
-# from airflow.sdk.definitions.asset.metadata import Metadata
-from airflow.sdk.metadata import Metadata
+from airflow.sdk.definitions.asset.metadata import Metadata
+# from airflow.sdk.metadata import Metadata
 from airflow.decorators import task
 from airflow.sdk import DAG
 
-inlet = Asset(name='asset_inlet')
-outlet = Asset(name='asset_outlet')
+outlet = Asset('asset_outlet')
+ten_days_ago = datetime.now() - timedelta(days=10)
 
 with DAG(
     dag_id="test_asset_event_producer",
-    start_date=datetime.datetime.min,
-    schedule=None,
+    start_date=ten_days_ago,
+    schedule='@daily',
     tags=["asset", "AIP-74"],
-    is_paused_upon_creation=False
+    is_paused_upon_creation=False,
+    catchup=True
 ) as dag:
     @task(outlets=[outlet])
-    def asset_with_extra_by_yield():
-        yield Metadata(outlet, {"hi": "bye"})
+    def asset_with_extra_by_yield2():
+        yield Metadata(outlet, {"hi": "bye2"})
 
-    asset_with_extra_by_yield()
+    asset_with_extra_by_yield2()
 
 with DAG(
     dag_id="test_asset_event_consumer",
     catchup=False,
-    start_date=datetime.datetime.min,
+    start_date=datetime.min,
     schedule=None,
     tags=["asset", "AIP-74"],
 ):
 
-    @task(inlets=[inlet])
+    @task(inlets=[outlet])
     def read_dataset_event(*, inlet_events=None):
-        for event in inlet_events[inlet][:-2]:
+        for event in inlet_events[outlet]:
+            print("Event: ", event)
             print(event.extra["hi"])
 
     read_dataset_event()
