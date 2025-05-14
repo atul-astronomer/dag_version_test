47bb06c596b16c858b6a66f5fbe949b0b7780b07
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2025-05-14 19:30:22 +0545

    change dag version

--- dags1/test_api_dag.py
+++ dags1/test_api_dag.py
@@ -12,7 +12,7 @@ dag = DAG(
 )
 
 hello_task = BashOperator(
-    task_id='test_task4',
+    task_id='test_task5',
     bash_command='echo "Hello World from Airflow!"',
     do_xcom_push = True,
     dag=dag,
